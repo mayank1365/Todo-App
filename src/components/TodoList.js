d102c67832cdcb1266ade78e88f373ea200bf691
mayank1365 <mayank1365@gmail.com>
2024-08-13 03:01:26 +0530

    Added functionality for todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -14,15 +15,36 @@ function TodoList() {
 
         setTodos(newTodos);
         
-        
     }
 
+    const completeTodo = id => {
+        let updatesTodos = todos.map(todo => {
+            if(todo,id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        });
 
+        setTodos(updatesTodos);
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+        setTodos(removeArr);
+    }
+
+    const editTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    }
 
     return (
         <div>
-        <h1>What's the Plan for Today?</h1>
-        <TodoForm onSubmit={addTodo}/>
+            <h1>What's the Plan for Today?</h1>
+            <TodoForm onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} editTodo={editTodo} />
         </div>
     )
 }
